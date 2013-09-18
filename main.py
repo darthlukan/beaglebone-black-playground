d79d72c8a2d91858cb6184adc24238194991b233
Brian Tomlinson <brian@smartm.no>
2013-09-18 20:42:56 +0200

    This would be so much easier if there were documentation... Seeing as how the names of things in the C code are not working...

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import time
 from Adafruit_BBIO import GPIO
 
 
-GPIO.setup("USR0", GPIO.OUT)
-GPIO.setup("USR1", GPIO.OUT)
-GPIO.setup("USR2", GPIO.OUT)
-GPIO.setup("USR3", GPIO.OUT)
+GPIO.setup("USR_0", GPIO.OUT)
+GPIO.setup("USR_1", GPIO.OUT)
+GPIO.setup("USR_2", GPIO.OUT)
+GPIO.setup("USR_3", GPIO.OUT)
 
 
 def blink_all():
     for i in range(0, 10):
 
-        GPIO.output("USR0", GPIO.HIGH)
-        GPIO.output("USR1", GPIO.HIGH)
-        GPIO.output("USR2", GPIO.HIGH)
-        GPIO.output("USR3", GPIO.HIGH)
+        GPIO.output("USR_0", GPIO.HIGH)
+        GPIO.output("USR_1", GPIO.HIGH)
+        GPIO.output("USR_2", GPIO.HIGH)
+        GPIO.output("USR_3", GPIO.HIGH)
         print "LEDs ON"
         time.sleep(2)
 
-        GPIO.output("USR0", GPIO.LOW)
-        GPIO.output("USR1", GPIO.LOW)
-        GPIO.output("USR2", GPIO.LOW)
-        GPIO.output("USR3", GPIO.LOW)
+        GPIO.output("USR_0", GPIO.LOW)
+        GPIO.output("USR_1", GPIO.LOW)
+        GPIO.output("USR_2", GPIO.LOW)
+        GPIO.output("USR_3", GPIO.LOW)
         print "LEDs OFF"
         time.sleep(2)
 
